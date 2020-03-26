2840b5264a0e48d660d7d16cedf83992de249567
dor6 <39131432+dor6@users.noreply.github.com>
2020-03-26 14:54:26 +0200

    added clear button

--- src/components/ClearButton/ClearButton.js
+++ src/components/ClearButton/ClearButton.js
@@ -1,15 +1,28 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import './ClearButton.css';
 
 
-export function ClearButton({clear}) {
+export function ClearButton({clear, renderDelay}) {
     const [close, setClose] = useState(false);
+    const [shouldRender, setShouldRender] = useState(false);
 
     const handleClick = useCallback(() => {
         setClose(true);
         setTimeout(() => clear(), 750);
     }, [setClose, clear]);
 
+
+    useEffect(()=> {
+        const timeout = setTimeout(()=> {
+            setShouldRender(true);
+        },renderDelay);
+
+        return () => clearTimeout(timeout);
+    },[setShouldRender, renderDelay]);
+
+    if(!shouldRender){
+        return '';
+    }
 
     let classes = ["ClearButton"];
     if(close)       classes.push("Close");
@@ -22,7 +35,8 @@ export function ClearButton({clear}) {
 }
 
 ClearButton.defaultProps = {
-    clear: () => {}
+    clear: () => {},
+    renderDelay: 5000
 };
 
 
